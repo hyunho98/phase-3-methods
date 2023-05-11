3623f6849e71169528fa357d00d592031ee721bb
Alex Lee <hyunho98@gmail.com>
2023-05-11 15:45:29 -0400

    commit tree 1c0a23cfb0bacae0b5e98b112eccaeb8e44b5a66

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    greet(name)
+end
+
+def add(a, b)
+    a + b
+end
+
+def halve(num)
+    if num.is_a? Integer
+        return num / 2
+    end
+    nil
+end
